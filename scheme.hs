cf464d0523f09a164dd01120580d9f70ae86420b
Magnus Morton <magnusmorton@gmail.com>
2011-10-26 17:50:21 +0100

    initial commit

---
+++ scheme.hs
@@ -0,0 +1,27 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal = Atom String
+			 | List [LispVal]
+			 | DottedList [LispVal] LispVal
+			 | Number Integer
+			 | String String
+			 | Bool Bool 
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+	Left err -> "No match: " ++ show err
+	Right val -> "Found value"
+	
+spaces :: Parser ()
+spaces = skipMany1 space
+	
+
+main :: IO ()
+main = do
+	args <- getArgs
+	putStrLn (readExpr (args !! 0))
