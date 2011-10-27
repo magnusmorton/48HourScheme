05ef2c958e5affe3c63b883f7821c81b53ff9ac6
Magnus Morton <magnusmorton@gmail.com>
2011-10-27 22:11:15 +0100

    chapter 3 done

--- scheme.hs
+++ scheme.hs
@@ -14,7 +14,7 @@ symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
 	Left err -> "No match: " ++ show err
 	Right val -> "Found value"
 	
@@ -32,12 +32,40 @@ parseAtom = do first <- letter <|> symbol
                rest <-many (letter <|> digit <|> symbol)
                let atom = [first] ++ rest
                return $ case atom of
-                          "#t" ->Bool True
-                          "#f" ->Bool False
-                          otherwise ->Atom atom
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit                 
+parseNumber = liftM (Number . read) $ many1 digit 
+
+parseExpr :: Parser LispVal
+parseExpr  = parseAtom
+ 		 <|> parseString
+		 <|> parseNumber
+		 <|> parseQuoted
+		 <|> do 
+			char '('
+			x <- (try parseList) <|> parseDottedList
+			char ')'
+			return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+	head <-	endBy parseExpr spaces
+	tail <- char '.' >> spaces >> parseExpr
+	return $ DottedList head tail
+	
+parseQuoted	:: Parser LispVal
+parseQuoted = do
+	char '\''
+	x <- parseExpr
+	return $ List [Atom "quote", x]
+
+               
 	
 
 main :: IO ()
